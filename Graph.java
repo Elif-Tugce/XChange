0e46d90cc562b43d4b26a22dd0948aebdf0c7f64
Elif Tuğçe Neziroğlu <tugce.neziroglu@ug.bilkent.edu.tr>
2023-12-10 16:28:55 +0300

    Name change in Currency class
    I've started the implementation of Graph class, and changed the name of the Currency class since it already exists in java.util package

---
+++ Graph.java
@@ -0,0 +1,21 @@
+package XChange;
+import java.util.Date;
+
+/**
+ * Graph
+ */
+public class Graph {
+    Date dateFrom;
+    Date dateTo;
+    CurrencyCalculation currencyFrom;
+    CurrencyCalculation currencyTo;
+    String graphName;
+    String graphDescription;
+    int graphImportance;
+    Date dateCreated;
+    String imageAddress;
+    double ratesToDollarByDay;
+
+
+    
+}
