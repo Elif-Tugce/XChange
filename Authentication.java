da94a7abee4feb51000ddc2155ade2fe3bc703e8
Mehmet Batuhan Neziroğlu <batuhan.neziroglu@ug.bilkent.edu.tr>
2023-12-10 21:04:05 +0300

    Some explanation for authentication
    For all three methods I made some basic logic and structure to make the real implementation easier

---
+++ Authentication.java
@@ -0,0 +1,33 @@
+/**
+ * Authentication
+ */
+public class Authentication {
+    public String userName;
+    public String currentPassword;
+    public String newPassword;
+    public String sequrityQuestionOne;
+    public String sequrityQuestionTwo;
+
+
+    public boolean userSignIn(){
+        if (userName == userName/*After the equal there should be something like database.searchForUsername(userName) which w'll return true if there is such a name*/ && currentPassword == currentPassword
+        /*Again there should be a search method l'ke database.searchForPassword(userName,password) wh'ch w'll return true if the userName has such a password */) {
+            return true;
+        }
+        return false;
+    }
+
+    public void userSignUp(){//WE SHOULD add all the necessary 'nformat'ons for a user to database in this\ we should create a new user in database
+
+    }
+
+    public void forgotPassword(){
+        if ( sequrityQuestionOne == sequrityQuestionOne && sequrityQuestionTwo == sequrityQuestionTwo
+        /*database.user.sequrityQuestionOne == sequrityQuestionOne && database.user.sequrityQuestionTwo == sequrityQuestionTwo */) {
+            currentPassword = newPassword;
+        }
+        /*OtherWise 't should g've an error message saying that your answers are incorrect */
+    }
+
+
+}
