b09cc32f69b729039fb262205280c6e5ca8e17dd
Ece <ecebulut1@gmail.com>
2023-12-11 18:33:11 +0300

    CurrencyCalculation Class

--- CurrencyCalculation.java
+++ CurrencyCalculation.java
@@ -1,11 +1,29 @@
 /**
  * CurrencyCalculation
  */
+
+import java.util.Date;
 public class CurrencyCalculation {
 
     private int currencyID;
     private String currencyCode;
     private String currencyName;
     private String currencyFlagPath;
+    private double rateToDollar;
+
+    public CurrencyCalculation (int currencyID, String currencyCode, String currencyName, String currencyFlagPath) {
+        this.currencyID = currencyID;
+        this.currencyCode = currencyCode;
+        this.currencyName = currencyName;
+        this.currencyFlagPath = currencyFlagPath;
+    }
+
+    public double [] getRateToDollar (Date dateFrom, Date dateTo) { 
+        return DatabaseConnection.getValuesByDate(currencyID, dateFrom, dateTo);
+    }
+   
+    public double getRateToDollarToday (){
+        return DatabaseConnection.getTodaysValue(currencyID);
+    }
     
 }
