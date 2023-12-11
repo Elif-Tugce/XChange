38ef39f0a209fda332f6a546a643ecb5c0fdf8f8
Mehmet Batuhan Neziroğlu <batuhan.neziroglu@ug.bilkent.edu.tr>
2023-12-11 12:20:04 +0300

    GUI
    Basic GUI Structure is ready

---
+++ Login.java
@@ -0,0 +1,13 @@
+public class Login {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LoginFrame loginFrame = new LoginFrame();
+        loginFrame.setVisible(true);
+        loginFrame.pack();
+        loginFrame.setLocationRelativeTo(null);
+        loginFrame.setResizable(false);
+    }
+    
+}
