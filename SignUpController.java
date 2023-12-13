066048d52f264ee14088df08f262a996a1331f00
Ece <ecebulut1@gmail.com>
2023-12-14 00:10:34 +0300

    Sign up Controller class created

---
+++ SignUpController.java
@@ -0,0 +1,64 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class SignUpController {
+
+    @FXML
+    private TextField favouriteColorTextField;
+
+    @FXML
+    private ImageView logoForFavouriteColor;
+
+    @FXML
+    private ImageView logoForMotherName;
+
+    @FXML
+    private ImageView logoForName;
+
+    @FXML
+    private ImageView logoForPassword;
+
+    @FXML
+    private ImageView logoForRepeatPassword;
+
+    @FXML
+    private ImageView logoForSurname;
+
+    @FXML
+    private ImageView logoForUsername;
+
+    @FXML
+    private ImageView mainLogo;
+
+    @FXML
+    private TextField motherNameTextField;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private PasswordField repeatPasswordField;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private TextField surnameTextField;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    void signUpButtonListener(MouseEvent event) {
+        
+
+    }
+
+}
