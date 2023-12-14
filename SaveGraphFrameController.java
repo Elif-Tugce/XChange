5fd6e74fbf135a0cb12ddc9ab3fb8c3e5eda20da
Elif Tuğçe Neziroğlu <tugce.neziroglu@ug.bilkent.edu.tr>
2023-12-14 23:17:48 +0300

    saveGraphFrameController created

---
+++ SaveGraphFrameController.java
@@ -0,0 +1,24 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class SaveGraphFrameController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void saveGraphButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
