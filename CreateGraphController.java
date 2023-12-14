0f739bee4c97756dc0e52e5110cb2d4359825eea
Elif Tuğçe Neziroğlu <tugce.neziroglu@ug.bilkent.edu.tr>
2023-12-14 23:02:27 +0300

    createGraphController Created

---
+++ CreateGraphController.java
@@ -0,0 +1,50 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class CreateGraphController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void amountDropDownAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void changeButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void convertedAmountDropDownAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void downloadButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void generateNowButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void saveButtonAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
