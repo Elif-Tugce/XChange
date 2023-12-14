b7bb345c6ac9cbf007c98fc0f90cb0ef0d265230
Ece <ecebulut1@gmail.com>
2023-12-14 20:20:13 +0300

    Forgot Password Controller is created

---
+++ ForgotPasswordController.java
@@ -0,0 +1,47 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class ForgotPasswordController {
+
+    @FXML
+    private TextField favouriteColorTextField;
+
+    @FXML
+    private ImageView logoForFavouriteColor;
+
+    @FXML
+    private ImageView logoForMotherName;
+
+    @FXML
+    private ImageView logoForName;
+
+    @FXML
+    private ImageView logoForPassword;
+
+    @FXML
+    private ImageView logoForRepeatPassword;
+
+    @FXML
+    private ImageView mainLogo;
+
+    @FXML
+    private TextField motherNameTextField;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private PasswordField repeatPasswordField;
+
+    @FXML
+    void changePasswordButtonListener(MouseEvent event) {
+
+    }
+
+}
