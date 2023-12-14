e687ca808a821576b47ac202f89b667a7369395d
Elif Tuğçe Neziroğlu <tugce.neziroglu@ug.bilkent.edu.tr>
2023-12-14 18:53:02 +0300

    CurrencyConverterController class is created

--- CurrencyConverterController.java
+++ CurrencyConverterController.java
@@ -1,2 +1,36 @@
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.MouseEvent;
+
 public class CurrencyConverterController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void amountDropDownAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void changeButtonListener(MouseEvent event) {
+
+    }
+
+    @FXML
+    void currencyConverterAmountListener(InputMethodEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
