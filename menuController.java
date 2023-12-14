f90c7dd583e70a9848f2de906395b561f11cd377
Elif Tuğçe Neziroğlu <tugce.neziroglu@ug.bilkent.edu.tr>
2023-12-15 00:50:15 +0300

    Login button is functional

--- menuController.java
+++ menuController.java
@@ -3,15 +3,18 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.SplitPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class menuController {
 
     private Scene scene;
+    private Stage stage;
 
     @FXML
     private ResourceBundle resources;
@@ -62,8 +65,12 @@ public class menuController {
     }
 
     @FXML
-    void logOutButtonListener(MouseEvent event) {
-
+    void logOutButtonListener(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("SignIn.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
