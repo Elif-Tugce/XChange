4791a4dca220c169864f3657350131e1d1df8b78
Elif Tuğçe Neziroğlu <tugce.neziroglu@ug.bilkent.edu.tr>
2023-12-14 20:17:42 +0300

    Switching between frames are available now
    switch between signIn, signUp, forgotPassword and signUpNow scenes are available

--- SignInController.java
+++ SignInController.java
@@ -1,14 +1,24 @@
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class SignInController {
 
+    private Stage stage;
+    private Scene scene;
+    
     @FXML
     private Button ForgotPasswordButton;
 
@@ -36,24 +46,32 @@ public class SignInController {
     @FXML
     private TextField usernameTextField;
 
-    @FXML
-    void SignInButtonListener(ActionEvent event) {
 
+    @FXML
+    void forgotPasswordButtonListener(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("ForgotPassword.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    void forgotPasswordButtonListener(MouseEvent event) {
-
+    void signInButtonListener(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("SidebarMenu.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
-    void signInButtonListener(MouseEvent event) {
-
-    }
-
-    @FXML
-    void signUpNowButtonListener(MouseEvent event) {
-
+    void signUpNowButtonListener(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("SignUp.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
--- SignUpController.java
+++ SignUpController.java
@@ -1,11 +1,22 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.scene.Node;
 
 public class SignUpController {
+
+    private Stage stage;
+    private Scene scene;
 
     @FXML
     private TextField favouriteColorTextField;
@@ -56,9 +67,13 @@ public class SignUpController {
     private TextField usernameTextField;
 
     @FXML
-    void signUpButtonListener(MouseEvent event) {
+    void signUpButtonListener(MouseEvent event) throws IOException {
         
-
+        Parent root = FXMLLoader.load(getClass().getResource("SignIn.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
