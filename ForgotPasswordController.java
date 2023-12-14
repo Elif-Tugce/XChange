68a793e10db76cb88b9d63112a45edb3f4f35117
Elif Tuğçe Neziroğlu <tugce.neziroglu@ug.bilkent.edu.tr>
2023-12-14 20:40:53 +0300

    frame connection continued

--- ForgotPasswordController.java
+++ ForgotPasswordController.java
@@ -1,11 +1,21 @@
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class ForgotPasswordController {
 
+    private Stage stage;
+    private Scene scene;
+    
     @FXML
     private TextField favouriteColorTextField;
 
@@ -40,8 +50,12 @@ public class ForgotPasswordController {
     private PasswordField repeatPasswordField;
 
     @FXML
-    void changePasswordButtonListener(MouseEvent event) {
-
+    void changePasswordButtonListener(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("SignIn.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
--- SignInController.java
+++ SignInController.java
@@ -62,6 +62,7 @@ public class SignInController {
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
+        stage.setMaximized(true);
         stage.show();
     }
 
