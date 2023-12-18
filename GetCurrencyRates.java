fe79afccc5b3a7d3996bffc8ce84a745eae4cb65
Emir Atlas <emiratlas11@gmail.com>
2023-12-18 13:41:07 +0300

    method to be implemented

--- GetCurrencyRates.java
+++ GetCurrencyRates.java
@@ -44,6 +44,13 @@ public class GetCurrencyRates {
 
         return dates;
     }
+
+    //Ansar you can implement this to save missing last days to database in the format of USD/cur
+    //There is a Database method getLatestDate which returns the latest date we have exchange rates
+    //Also Database.getCurrencies returns codes of all currencies we have as an arraylist
+    public static void saveHistoricalData() {
+
+    }
     
 
 }
