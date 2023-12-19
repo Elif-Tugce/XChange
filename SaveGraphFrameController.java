4e10e997faa68b4d80ff0c973c61af2ec1c4d383
Emir Atlas <emiratlas11@gmail.com>
2023-12-19 21:08:22 +0300

    some code arrangement

--- SaveGraphFrameController.java
+++ SaveGraphFrameController.java
@@ -2,30 +2,14 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import javax.swing.plaf.basic.BasicInternalFrameTitlePane.CloseAction;
-
 import com.microsoft.sqlserver.jdbc.StringUtils;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.shape.ClosePath;
-import javafx.stage.Stage;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 
 public class SaveGraphFrameController {
-
-    //private Stage stage;
-    //private Scene scene;
 
     @FXML
     private TextField graphNameTextField;
@@ -66,11 +50,5 @@ public class SaveGraphFrameController {
         
         
     }
-
-    @FXML
-    void initialize() {
-
-    }
-
 
 }
